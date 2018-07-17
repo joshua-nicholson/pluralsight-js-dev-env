e52ac766dcf328a3bd6b82dbb1f6f0bc699e7a0c
Josh Nicholson <joshwnicholson@gmail.com>
2018-07-16 22:32:05 -0400

    Added several configs for production: minify, bundle splitting, cache busting, logging, mock API toggle

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 import jsf from 'json-schema-faker';
 import {schema} from './mockDataSchema';
 import fs from 'fs';
--- src/index.test.js
+++ src/index.test.js
@@ -8,9 +8,9 @@ describe('Our first test', () => {
     });
 });
 
-describe('index.html', () => {
+describe('index.ejs', () => {
     it('should have h1 that says Users', (done) => {
-        const index = fs.readFileSync('./src/index.html', "utf-8");
+        const index = fs.readFileSync('./src/index.ejs', "utf-8");
         jsdom.env(index, function(err, window) {
             const h1 = window.document.getElementsByTagName('h1')[0];
             expect(h1.innerHTML).to.equal("Users");
