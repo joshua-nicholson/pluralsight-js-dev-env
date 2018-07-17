9d7eb7fea2a8b52ce8450db30f0947ba24e9ab22
Josh Nicholson <joshwnicholson@gmail.com>
2018-07-16 23:04:24 -0400

    Added config for deployment using Heroku and Surge

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://ancient-brushlands-58112.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name) {
